e26ae6af2d3ac202944b2742f3d11a70dba4703d
ljyockey <leoyockey@gmail.com>
2019-04-23 21:33:08 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+console.log('hello, world!');
+
+ReactDOM.render(<Index />, document.getElementById('index'));
