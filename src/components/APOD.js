9f558d6b0983c2f6fd9ce0559fffa2852eb1026c
ljyockey <leoyockey@gmail.com>
2019-05-21 21:05:59 -0700

    setting up mock data

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -29,17 +29,34 @@ export class APOD extends Component {
     this.onStateChangeSubmit = this.onStateChangeSubmit.bind(this)
   }
 
+  getPhoto () {
+    this.setState({
+      apodData: {
+        copyright: "Crni Vrh ObsU. Ljubljana",
+        date: "1997-02-12",
+        explanation: "Comet Hale-Bopp has quite a tail to tell already.\r This remarkable comet\r was first discovered in 1995, even\r before Comet Hyakutake. Since then,\r this erupting snowball continues\r to fall into our inner Solar System\r and is starting to put on quite a show. Comets\r have been known throughout history to show tails that spread across\r the sky.  In the above picture, the blue stream is the ion tail\r which consists of ions pushed away from the comet's head\r by the solar wind.\r The ion tail always points directly\r away from the Sun. Comet Hale-Bopp\r is now visible in the morning sky,\r moving a few degrees each day. Comet Hale-Bopp\r is expected to be at its best and brightest in late March and early April.\r",
+        hdurl: "https://apod.nasa.gov/apod/image/9702/halebopp1_cvo_big.gif",
+        media_type: "image",
+        service_version: "v1",
+        title: "Comet Hale-Bopp Develops a Tail",
+        url: "https://apod.nasa.gov/apod/image/9702/halebopp1_cvo.gif"
+    }
+    })
+  }
+
   onStateChangeSubmit (event) {
+    if (this.state.hasResult) this.getPhoto()
     this.setState({hasResult: !this.state.hasResult})
   }
 
   render () {
+    console.log(this.state)
     return (
       <section>
         <Header />
         <main role='main'>
           {this.state.hasResult
-          ? <Result stateChangeCallback={this.onStateChangeSubmit} />
+          ? <Result stateChangeCallback={this.onStateChangeSubmit} apodData={this.state.apodData} />
           : <Search stateChangeCallback={this.onStateChangeSubmit} />}
         </main>
       </section>  
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 export default function Search (props) {
-    return <button onClick={props.stateChangeCallback}>Search</button>
+    const date = new Date()
+    const today = `${date.getFullYear()}-${(`0${(date.getMonth() + 1)}`).slice(-2)}-${date.getDate()}`
+    return (
+        <form onSubmit={props.stateChangeCallback}>
+            <TextField
+                id="date"
+                label="Pick a date between X and now"
+                type="date"
+                defaultValue={today}
+                InputLabelProps={{
+                shrink: true,
+                }}
+            />
+            <button>Search</button>
+        </form>
+    )
 }
